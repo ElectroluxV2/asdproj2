3343c67cc202c771bd598eb5bc20aae8509a8874
electroluxv2 <electroluxv2@gmail.com>
2021-12-22 02:27:46 +0100

    Refactored huffman grouping algorithm

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "heap.h"
 #include "queue.h"
 #include "occurrencesCount.h"
+#include "huffman.h"
 
 void addGroupTBD(group* array, const unsigned long long index, char* value, const unsigned long long count) {
     group created = {
@@ -45,143 +46,77 @@ void tryToMatchStringWithDictionary(char** dictionary, const char* string, unsig
 }
 
 int main() {
-    unsigned long long* occurrences = countOccurrencesOfCharactersInFile("./../input");
-    // n
-    unsigned long long totalUniqueCharacters = countTotalNonZeroOccurrences(occurrences) + 1;
-    // There always will be 2n - 1 output groups
-    unsigned long long totalPossibleGroupsInStore = totalUniqueCharacters + totalUniqueCharacters - 1;
+    char** huffmanDictionary = getHuffmanDictionaryForFile("./../input");
 
-    // First n groups will be created during counting phase
-    unsigned long long lastFreeGroupsIndex = 0;
-    group* groups = makeGroupsFromOccurrences(occurrences, totalPossibleGroupsInStore, &lastFreeGroupsIndex);
-    free(occurrences);
-
-    // Our queue will never exceed n
-    group** queue = malloc(sizeof(group*) * totalUniqueCharacters);
-    unsigned long long currentQueueLength = totalUniqueCharacters;
-
-    // Insert pointers to groups
-    for (unsigned long long index = 0; index < totalUniqueCharacters; index++) {
-        *(queue + index) = groups + index;
-    }
-
-    // Every queue must be built on top of heap
-    buildHeap(queue, currentQueueLength);
-
-    // Next n - 1 groups are calculated here and settled at next free index starting from n
-    for (unsigned long long i = 0; i < totalUniqueCharacters - 1; i++) {
-        group* left = extract(queue, &currentQueueLength);
-        group* right = extract(queue, &currentQueueLength);
-
-        //printf("left: %s:%lld, right: %s:%lld\n", (char*) left->value, left->count, (char*) right->value, right->count);
-
-        group* pointerToLatestFreeGroup = (groups + lastFreeGroupsIndex++);
-        pointerToLatestFreeGroup->value = combineStrings(left->value, right->value);
-        pointerToLatestFreeGroup->count = left->count + right->count;
-
-        left->parent = pointerToLatestFreeGroup;
-        left->isRightChild = false;
-
-        right->parent = pointerToLatestFreeGroup;
-        right->isRightChild = true;
-
-        insert(queue, &currentQueueLength, pointerToLatestFreeGroup);
-    }
-
-    // No more needed
-    free(queue);
-
-    char** dictionary = malloc(sizeof (char**) * ('z' - 'a' + 1 + 1)); // dictionary as an array of pointers
-                                                                            // to strings representing char codes
-
-    for (int i = 0; i < 'z' - 'a' + 1; i++) {
-        *(dictionary + i) = NULL;
-    }
-    // Decode bit values for first n groups
-    for (unsigned long long index = 0; index < totalUniqueCharacters; index++) {
-        group* currentGroup = groups + index;
-        char characterToBeEncoded = *currentGroup->value;
-
-        unsigned int dictionaryIndex = 0;
-        if (characterToBeEncoded == '\000') dictionaryIndex = 'z' - 'a' + 1; // last index of dictionary
-        else dictionaryIndex = (int)(characterToBeEncoded - 'a');
-
-        *(dictionary + dictionaryIndex) = calloc(sizeof(char), 10); // TODO: better way of estimating how many bits can be
-
-        // This will get byte value in reversed order
-        while (currentGroup->parent != NULL) {
-            strcat(*(dictionary + dictionaryIndex), currentGroup->isRightChild ? "1" : "0");
-            currentGroup = currentGroup->parent;
-        }
-        stringReverse(*(dictionary + dictionaryIndex));
-        //printf("Character: %c byteValue: %s\n", characterToBeEncoded, *(dictionary + dictionaryIndex));
-    }
-
-    for (unsigned long long index = 0; index < totalPossibleGroupsInStore; index++) {
-        free((groups + index)->value);
-    }
-
-    free(groups);
-
-
-    FILE* file = fopen("./../input", "r");
-    unsigned long size = calculateSizeOfEncodedInput(dictionary, file);
-    //printf("Size: %lu\n", size);
-
-    char* binaryString = calloc(sizeof(char), size);
-
-    size_t n = 0;
-    fseek(file, 0, SEEK_END);
-    long f_size = ftell(file);
-    fseek(file, 0, SEEK_SET);
-    int c;
-    while ((c = fgetc(file)) != EOF) {
-        unsigned int dictionaryIndex = 0;
-        dictionaryIndex = c - 'a';
-        //printf("Character: %c byteValue: %s\n", c, *(dictionary + dictionaryIndex));
-        strcat(binaryString, *(dictionary + dictionaryIndex));
-    }
-    strcat(binaryString, *(dictionary + 'z' - 'a' + 1));
+    printHuffmanDirectory(huffmanDictionary);
 
 
 
-    FILE* output = fopen("./../data.bin", "wb");
-    if (file == NULL) return 1;
-    for (int i = 0; i < strlen(binaryString); i+=8) {
-        char part[9];
-        strncpy(part, binaryString + i, 8);
-        part[8] = '\0';
-
-        unsigned char number = strtol(part, NULL, 2);
-
-        printf("%s: %d\n", part, number);
-        fputc(number, output);
-    }
-    fclose(output);
-
-    output = fopen("./../data.bin", "rb");
-    if (output == NULL) return 1;
-
-    // Trzeba zapisać w nagłówku ile będzie liczb po 8 bitów = rozmiar pliku
-    for (int i = 0; i < 30; i++) {
-        unsigned char number;
-        fread(&number, sizeof(unsigned char), 1, output);
-
-        for (int bit = 8; bit; --bit) {  // count from 8 to 1
-            putchar(number & (1 << (bit - 1)) ? '1' : '0');
-        }
-
-        printf(": %d\n", number);
+    freeHuffmanDictionary(huffmanDictionary);
+    free(huffmanDictionary);
 
 
-    }
-    fclose(output);
-
-    for (int i = 0; i < 'z' - 'a' + 1; i++) {
-        if (*(dictionary + i) != NULL) free(*(dictionary + i));
-    }
-
-    free(dictionary);
-    free(binaryString);
-    fclose(file);
+//
+//
+//
+//    FILE* file = fopen("./../input", "r");
+//    unsigned long size = calculateSizeOfEncodedInput(dictionary, file);
+//    //printf("Size: %lu\n", size);
+//
+//    char* binaryString = calloc(sizeof(char), size);
+//
+//    size_t n = 0;
+//    fseek(file, 0, SEEK_END);
+//    long f_size = ftell(file);
+//    fseek(file, 0, SEEK_SET);
+//    int c;
+//    while ((c = fgetc(file)) != EOF) {
+//        unsigned int dictionaryIndex = 0;
+//        dictionaryIndex = c - 'a';
+//        //printf("Character: %c byteValue: %s\n", c, *(dictionary + dictionaryIndex));
+//        strcat(binaryString, *(dictionary + dictionaryIndex));
+//    }
+//    strcat(binaryString, *(dictionary + 'z' - 'a' + 1));
+//
+//
+//
+//    FILE* output = fopen("./../data.bin", "wb");
+//    if (file == NULL) return 1;
+//    for (int i = 0; i < strlen(binaryString); i+=8) {
+//        char part[9];
+//        strncpy(part, binaryString + i, 8);
+//        part[8] = '\0';
+//
+//        unsigned char number = strtol(part, NULL, 2);
+//
+//        printf("%s: %d\n", part, number);
+//        fputc(number, output);
+//    }
+//    fclose(output);
+//
+//    output = fopen("./../data.bin", "rb");
+//    if (output == NULL) return 1;
+//
+//    // Trzeba zapisać w nagłówku ile będzie liczb po 8 bitów = rozmiar pliku
+//    for (int i = 0; i < 30; i++) {
+//        unsigned char number;
+//        fread(&number, sizeof(unsigned char), 1, output);
+//
+//        for (int bit = 8; bit; --bit) {  // count from 8 to 1
+//            putchar(number & (1 << (bit - 1)) ? '1' : '0');
+//        }
+//
+//        printf(": %d\n", number);
+//
+//
+//    }
+//    fclose(output);
+//
+//    for (int i = 0; i < 'z' - 'a' + 1; i++) {
+//        if (*(dictionary + i) != NULL) free(*(dictionary + i));
+//    }
+//
+//    free(dictionary);
+//    free(binaryString);
+//    fclose(file);
 }
