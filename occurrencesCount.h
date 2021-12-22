c13ffc4489c4d4b016676c4a327498495f21b881
electroluxv2 <electroluxv2@gmail.com>
2021-12-22 02:28:18 +0100

    Fixed negative value of total element count

--- occurrencesCount.h
+++ occurrencesCount.h
@@ -8,7 +8,7 @@
 #include "shared.h"
 
 // Total supported ascii keys
-static const unsigned TOTAL_POSSIBLE_KEYS_IN_OCCURRENCES_MAP = ' ' - '~';
+static const unsigned TOTAL_POSSIBLE_KEYS_IN_OCCURRENCES_MAP = '~' - ' ' + 1;
 // Amount we need to subtract from evey key to start indexing from 0 ak. int value of the lowest possible char key.
 static const unsigned KEY_OFFSET_IN_OCCURRENCES_MAP = ' ';
 
@@ -43,10 +43,13 @@ unsigned long long* countOccurrencesOfCharactersInFile(const char* fileName) {
     return occurrences;
 }
 
-char* convertIntToCharacterSingletonArray(int character) {
-    const char c = (char) (character + ' ');
+char convertIntegerToCharacterKey(int integer) {
+    return (char) (integer + KEY_OFFSET_IN_OCCURRENCES_MAP);
+}
+
+char* convertIntToCharacterSingletonArray(char character) {
     char* dynamic = malloc(sizeof(char) * 2);
-    *dynamic = c;
+    *dynamic = character;
     *(dynamic + 1) = '\0';
     return dynamic;
 }
@@ -58,13 +61,13 @@ group* makeGroupsFromOccurrences(const unsigned long long int *occurrences, unsi
         if ((*(occurrences + i)) == 0) continue; // There is no other way of skipping empty values
 
         group* currentGroup = (groups + (*lastFreeGroupsIndex)++);
-        currentGroup->value = convertIntToCharacterSingletonArray(i);
+        currentGroup->value = convertIntToCharacterSingletonArray(convertIntegerToCharacterKey(i));
         currentGroup->count = *(occurrences + i);
     }
 
     // End of context char '\0'
     group* trailingNull = (groups + (*lastFreeGroupsIndex)++);
-    trailingNull->value = convertIntToCharacterSingletonArray('\0' - ' ');
+    trailingNull->value = convertIntToCharacterSingletonArray('\0');
     trailingNull->count = 1;
 
     return groups;
