73a0316fdff86c94fb63ded0305c49d44dac805e
electroluxv2 <electroluxv2@gmail.com>
2022-01-03 14:44:54 +0100

    Simple cli

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include "encode.h"
 #include "decode.h"
 
-int main() {
-//    encode("./../input", "./../encoded");
-    decode("./../encoded", "./../decoded");
+int main(int argc, const char** argv) {
+    if (argc < 4) {
+        printf("Usage [dehumanise/humanise] [input] [output]\n");
+        return 0;
+    }
+
+    if (strcmp(*(argv + 1), "dehumanise") == 0) {
+        if (encode(*(argv + 2), *(argv + 3))) {
+            printf("Dehumanised\n");
+        } else {
+            printf("Failed to dehumanise\n");
+        }
+    } else {
+        if (decode(*(argv + 2), *(argv + 3))) {
+            printf("Humanised\n");
+        } else {
+            printf("Failed to humanise\n");
+        }
+    }
 }
